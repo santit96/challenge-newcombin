5646cd029dd9c3a0a480ddc0c87a4aceddfa7647
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2022-11-18 18:33:46 -0300

    Add date filter on transactions view

--- api/models/transaction.py
+++ api/models/transaction.py
@@ -2,13 +2,26 @@ from django.db import models
 from api.models.payable import Payable
 from api.models.payment_method_enum import PaymentMethodEnum
 from django.core.exceptions import ValidationError
+from django.db.models import Count, Sum
+from django.db.models.functions import TruncDay
+
+
+class TransactionManager(models.Manager):
+    def group_by_day(self):
+        return self.annotate(
+            day=TruncDay('payment_date')
+        ).values('day').annotate(count=Count('id'), total_amount=Sum('amount')).values('day', 'count', 'total_amount')
 
 class Transaction(models.Model):
-    payment_method = models.CharField(max_length=25, choices=PaymentMethodEnum.choices)
+    payment_method = models.CharField(
+        max_length=25, choices=PaymentMethodEnum.choices)
     card_number = models.IntegerField()
     amount = models.IntegerField()
-    barcode = models.ForeignKey(Payable, on_delete=models.PROTECT, related_name='payments')
+    barcode = models.ForeignKey(
+        Payable, on_delete=models.PROTECT, related_name='payments')
     payment_date = models.DateField()
+
+    objects = TransactionManager()
 
     def save(self, *args, **kwargs):
         if self.barcode.status != 'pending':
--- api/views/transactions_view.py
+++ api/views/transactions_view.py
@@ -1,30 +1,18 @@
 from api.models.transaction import Transaction
 from api.serializers.transaction_serializer import TransactionSerializer
 from api.serializers.transaction_list_serializer import TransactionListSerializer
-from rest_framework import generics
-from django.db.models import Count, Sum
-from django.db.models.functions import TruncDay
-from rest_framework.response import Response
-from rest_framework.filters import SearchFilter
-from django.core.exceptions import ValidationError
-
-
-class TransactionsFilter(SearchFilter):
-
-    def filter_queryset(self, request, queryset, view):
-        queryset = super().filter_queryset(request, queryset, view)
-        return queryset.annotate(
-            day=TruncDay('payment_date')
-        ).values('day').annotate(count=Count('id'), total_amount=Sum('amount')).values('day', 'count', 'total_amount')
-
+from rest_framework import generics 
 
 class TransactionsView(generics.ListCreateAPIView):
-    queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     list_serializer_class = TransactionListSerializer
-    filter_backends = TransactionsFilter
+    filterset_fields = {
+        'payment_date':['gte', 'lte', 'exact', 'gt', 'lt'],
+    }
+
+    def get_queryset(self):
+        return Transaction.objects.group_by_day()
 
     def list(self, request):
-        queryset = self.filter_backends().filter_queryset(request, self.queryset, self)
-        #data = self.list_serializer_class(queryset, many=True).data
-        return Response(queryset)
+        self.serializer_class = TransactionListSerializer
+        return super().list(request)
